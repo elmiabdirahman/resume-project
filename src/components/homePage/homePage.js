0c5379e578e5a4690a8eb282439a3a17b4901933
Elmi Elmi <elmielmi522@gmail.com>
2020-04-17 02:50:01 -0500

    set up the home page info

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import resumeNav from '../resumeNav/resumeNav';
+import { connect } from 'react-redux';
 
 class homePage extends Component {
+
+    componentDidMount() {
+        this.getHomePage();
+    }
+
+    getHomePage = () => {
+        this.props.dispatch({ type: 'GET_HOME' })
+    }
+    
     render() {
         return (
             <div>
                 <h1>Homeeee</h1>
+                {JSON.stringify(this.props.homePageReducer)}
             </div>
         );
     }
 }
+const putReduxStateOnProps = (reduxStore) => ({
+    homePageReducer: reduxStore.homePageReducer
+});
 
-export default homePage;
+export default connect(putReduxStateOnProps) (homePage);
