589dcde24a9d3b4660d034d6498ceee6f2284321
Elmi Elmi <elmielmi522@gmail.com>
2020-04-30 03:25:38 -0500

    created email page

---
+++ src/components/PersonalEmail/PersonalEmail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PersonalEmail extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Personal Email</h1>
+            </div>
+        );
+    }
+}
+
+export default PersonalEmail;
