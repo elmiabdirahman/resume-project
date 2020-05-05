22f0be3831230f7c317733ce7a7ba5c6b19347cc
Elmi Elmi <elmielmi522@gmail.com>
2020-05-05 02:36:26 -0500

    added skills in about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -22,13 +22,18 @@ getAbouPage = () => {
             This about page is for anyone to read!
             Hello
           </h1>
-          {JSON.stringify(this.props.aboutPageReducer)}
+          {/* {JSON.stringify(this.props.aboutPageReducer)} */}
           <div>
+            <h1>About Me</h1>
             {this.props.aboutPageReducer.map( (item) =>
             <div>
               <h3>{item.description}</h3>
             </div>
             )}
+          </div>
+          <div>
+            <h1>Skills</h1>
+            <h4>HTML</h4><h4>CSS</h4><h4>Javascript</h4><h4>jQuery</h4><h4>Node.js</h4><h4>React.js</h4><h4>PostgreSql</h4>
           </div>
         </div>
       </div>
